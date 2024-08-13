5080b3dc210362d18f1cd57feeb7762d5839025d
your Name <puneethgowda91081@gmail.com>
2024-08-13 21:30:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+mongoose.connect("mongodb://127.0.0.1:27017/Database");
+var db = mongoose.connection;
+db.on("error", () => console.log("error in connecting"));
+db.once("open", () => console.log("connected to database"));
+app.post("/sign_up", (req, res) => {
+  var name = req.body.name;
+  var age = req.body.age;
+  var email = req.body.email;
+  var phno = req.body.phno;
+  var gender = req.body.gender;
+  var password = req.body.password;
+  var data = {
+    name: name,
+    age: age,
+    email: email,
+    phno: phno,
+    gender: gender,
+    password: password,
+  };
+  db.collection("users").insertOne(data, (err, collection) => {
+    if (err) {
+      throw err;
+    }
+    console.log("record is inserted");
+  });
+  return res.redirect("signup_successful.html");
+});
+
+app
+  .get("/", (req, res) => {
+    res.set({
+      "Allow-acces-Allow-Origin": "*",
+    });
+    return res.redirect("index.html");
+  })
+  .listen(3000);
+console.log("listening on port 3000");
